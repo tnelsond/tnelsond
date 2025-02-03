ef3065587a87609ef7436dd179cb3ff17ee4680a
tnelsond <tnelsond@gmail.com>
2025-02-03 15:39:11 -0600

    added peakslab

---
+++ peakslab/dict.js
@@ -0,0 +1,163 @@
+/*
+  2022-09-19
+
+  The author disclaims copyright to this source code.  In place of a
+  legal notice, here is a blessing:
+
+  *   May you do good and not evil.
+  *   May you find forgiveness for yourself and forgive others.
+  *   May you share freely, never taking more than you give.
+
+  ***********************************************************************
+
+  A basic demonstration of the SQLite3 "OO#1" API.
+*/
+'use strict';
+(function(){
+  /**
+     Set up our output channel differently depending
+     on whether we are running in a worker thread or
+     the main (UI) thread.
+  */
+  let logHtml;
+  if(globalThis.window === globalThis /* UI thread */){
+    console.log("Running demo from main UI thread.");
+    logHtml = function(cssClass,...args){
+      const ln = document.createElement('div');
+      if(cssClass) ln.classList.add(cssClass);
+      ln.append(document.createTextNode(args.join(' ')));
+      document.body.append(ln);
+    };
+  }else{ /* Worker thread */
+    console.log("Running demo from Worker thread.");
+    logHtml = function(cssClass,...args){
+      postMessage({
+        type:'log',
+        payload:{cssClass, args}
+      });
+    };
+  }
+  const dstyle = {};
+  dstyle["kh"] = '<h3 class="hent">###</h3>';
+  dstyle["h97"] = '<p style="color: #240074"><span class="tag is-small">Headley 97</span><br>###</p>';
+  dstyle["h77"] = '<p style="color: #007400"><span class="tag is-small">Headley 77</span><br>###</p>';
+  dstyle["khdef"] = '<p><span class="tag is-small">Chuon Nath 2023</span><br>###</p>';
+  dstyle["khph"] = 
+  dstyle["khpos"] = '<p style="color: #740000">###</p>';
+  dstyle["csea"] = '<span style="color: red">###</span><sup>csea</sup> ';
+  dstyle["ckhsv"] = '<span style="color: purple">###</span><sup>ckhsv</sup> ';
+  dstyle["ckhov"] = '<span style="color: green">###</span><sup>ckhov</sup> ';
+  dstyle["cchrist"] = '<span style="color: blue">###</span><sup>cchrist</sup> ';
+
+  onmessage = (e) => {
+    console.log("Worker: Message received from main script#: " + e.data);
+    console.log("Worker: Posting message back to main script");
+ //  addEntry(e.data);
+      if(e.data.startsWith(".init: ")){
+	db.exec({
+        sql: 'PRAGMA table_info(' + e.data.substring(7) + ');',
+        rowMode: 'array', // 'array' (default), 'object', or 'stmt'
+        callback: function(row){
+ 	  ++this.counter;
+          logHtml("init", row[1]);
+        }.bind({counter: 0})
+      });
+      }else{
+      db.exec({
+        sql: e.data,
+        rowMode: 'array', // 'array' (default), 'object', or 'stmt'
+        callback: function(row){
+ 	  ++this.counter;
+	  const lopokup = e.data.match(/SELECT (.*) FROM/)[1].split(", ");
+	  console.log(lopokup);
+	  let res = "";
+	  for(let i=0; i<row.length; ++i){
+	 	if(row[i])
+			res += dstyle[lopokup[i]].replace("###", row[i]);
+	  }
+          addEntry(''+res+'');
+          //addEntry(row.join('</div><div class="col">'));
+        }.bind({counter: 0})
+      });
+    };
+  }
+
+  const log = (...args)=>logHtml('',...args);
+  const addEntry = (...args)=>logHtml('entry',...args);
+  const warn = (...args)=>logHtml('bg-warning',...args);
+  const error = (...args)=>logHtml('bd-error',...args);
+  var db;
+  const demo1 = function(sqlite3, dbUrl, immutable=false) {
+    fetch(dbUrl)
+    .then(res => res.arrayBuffer())
+    .then(arrayBuffer => {
+        if (!immutable) {
+            arrayBuffer.resizeable = true;
+        }
+        const p = sqlite3.wasm.allocFromTypedArray(arrayBuffer);
+	db = new sqlite3.oo1.DB();
+        let deserialize_flags =
+            sqlite3.capi.SQLITE_DESERIALIZE_FREEONCLOSE;
+        if (!immutable) {
+            deserialize_flags |= sqlite3.capi.SQLITE_DESERIALIZE_RESIZEABLE;
+        }
+        const rc = sqlite3.capi.sqlite3_deserialize(
+            db.pointer, 'main', p, arrayBuffer.byteLength, arrayBuffer.byteLength, deserialize_flags);
+        db.checkRc(rc);
+
+	db.exec({
+        sql: "SELECT name FROM sqlite_master WHERE type='table'",
+        rowMode: 'array', // 'array' (default), 'object', or 'stmt'
+        callback: function(row){
+          logHtml("dictlist",
+    '<option value="' + row + '">' + row + '</option>');
+        }.bind({counter: 0})
+      });
+
+      logHtml("refresh", "");
+
+//        xplorer.setDb(db);
+     });
+   }
+
+  log("Loading and initializing sqlite3 module...");
+  if(globalThis.window!==globalThis) /*worker thread*/{
+    /*
+      If sqlite3.js is in a directory other than this script, in order
+      to get sqlite3.js to resolve sqlite3.wasm properly, we have to
+      explicitly tell it where sqlite3.js is being loaded from. We do
+      that by passing the `sqlite3.dir=theDirName` URL argument to
+      _this_ script. That URL argument will be seen by the JS/WASM
+      loader and it will adjust the sqlite3.wasm path accordingly. If
+      sqlite3.js/.wasm are in the same directory as this script then
+      that's not needed.
+
+      URL arguments passed as part of the filename via importScripts()
+      are simply lost, and such scripts see the globalThis.location of
+      _this_ script.
+    */
+    let sqlite3Js = 'sqlite3.js';
+    const urlParams = new URL(globalThis.location.href).searchParams;
+    log("urlParams: " + urlParams);
+    if(urlParams.has('sqlite3.dir')){
+      sqlite3Js = urlParams.get('sqlite3.dir') + '/' + sqlite3Js;
+      log("sqlite3: " + sqlite3Js);
+    }
+    importScripts(sqlite3Js);
+  }
+  globalThis.sqlite3InitModule({
+    /* We can redirect any stdout/stderr from the module like so, but
+       note that doing so makes use of Emscripten-isms, not
+       well-defined sqlite APIs. */
+    print: log,
+    printErr: error
+  }).then(function(sqlite3){
+    //console.log('sqlite3 =',sqlite3);
+    log("Done initializing. Running demo...");
+    try {
+      demo1(sqlite3, 'tdict3.db');
+    }catch(e){
+      error("Exception:",e.message);
+    }
+  });
+})();
---
+++ peakslab/dictsw.js
@@ -0,0 +1,40 @@
+let cache_name = 'tnedict 0.0.4';
+let urls_to_cache = [
+ '/index.html',
+ '/dict.js',
+ '/tdict3.db',
+ '/jswasm/sqlite3.js',
+ '/jswasm/sqlite3.wasm'];
+self.addEventListener('install', (e) => {
+ e.waitUntil(caches.open(cache_name).then((cache) => {
+  return cache.addAll(urls_to_cache)
+ }) )
+})
+
+/*
+self.addEventListener('fetch', (e) => {
+ e.respondWith(caches.match(e.request).then((response) => {
+  if(response)
+   return response
+  else
+   return fetch(e.request)
+ }) )
+})*/
+
+/*
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    (async () => {
+      const r = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (r) {
+        return r;
+      const response = await fetch(e.request);
+      const cache = await caches.open(cache_name);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })(),
+  );
+});
+*/
